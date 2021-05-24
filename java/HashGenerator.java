a09ae2853871b5fb1189819cfa00dfe39c06ec9e
dominik-wisniewski <42099601+dominik-wisniewski@users.noreply.github.com>
2021-05-24 18:22:20 +0200

    Update and rename Argon2Generator.java to HashGenerator.java

---
+++ java/HashGenerator.java
@@ -0,0 +1,38 @@
+import org.bouncycastle.crypto.generators.Argon2BytesGenerator;
+import org.bouncycastle.crypto.generators.BCrypt;
+import org.bouncycastle.crypto.params.Argon2Parameters;
+import java.util.Base64;
+import java.nio.charset.StandardCharsets;
+
+public class HashGenerator {
+
+	public static String generateBcrypt(String password, String salt, int costFactor) {
+
+		byte[] pass_arr = password.getBytes();
+		byte[] salt_arr = salt.getBytes();
+		byte[] hash = BCrypt.generate(pass_arr, salt_arr, costFactor);
+
+		return Base64.getEncoder().encodeToString(hash);
+
+	}
+	
+	public static String generateArgon2id (String password, String salt, int opsLimit, int memLimit, int outputLength, int parallelism) {
+
+	byte[] salt_arr = salt.getBytes();
+
+	Argon2Parameters.Builder builder = new Argon2Parameters.Builder(Argon2Parameters.ARGON2_id)
+		.withVersion(Argon2Parameters.ARGON2_VERSION_13) // 19
+		.withIterations(opsLimit)
+		.withMemoryAsKB(memLimit)
+		.withParallelism(parallelism)
+		.withSalt(salt_arr);
+	Argon2BytesGenerator gen = new Argon2BytesGenerator();
+	gen.init(builder.build());
+	byte[] result = new byte[outputLength];
+	gen.generateBytes(password.getBytes(StandardCharsets.UTF_8), result, 0, result.length);
+
+	return Base64.getEncoder().encodeToString(result);
+
+	}
+
+}
